bfa770ef1a2fc4312709536b2b74f9d1f3d25619
annasowy <59039409+annasowy@users.noreply.github.com>
2022-06-05 22:49:40 -0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -46,29 +46,6 @@ from CTE
 left join main on main.month = cte.cte_start_date
 order by MonthYear desc
 
--- yearly revenue
-
-with order_sum as (
-select 
-	order_id, 
-	sum(price) as total_price,
-	sum(freight_value) as shipping,
-	sum(price) + sum(freight_value) as total
-from order_items$ 
-group by order_id
-)
-
-select 
-	year(orders$.order_approved_at) as 'year',
-	sum(order_sum.total_price) as revenue
-from order_sum 
-left join orders$ on orders$.order_id = order_sum.order_id
-where 1=1
-and order_status = 'delivered'
-and orders$.order_approved_at is not null
-group by year(orders$.order_approved_at)
-order by 'year' desc
-
 -- avg order spending
 
 with order_sum as (
@@ -110,9 +87,10 @@ main as (
 select 
 	orders$.order_id,
 	total_price,
-	year(order_approved_at) as 'Year',
-	month(order_approved_at) as 'Month',
-	DATEPART(hour, order_approved_at) as 'Hour',
+	year(order_approved_at) as [year],
+	month(order_approved_at) as [Month],
+	DATEPART(ISO_WEEK, order_approved_at) as [Week],
+	DATEPART(hour, order_approved_at) as [Hour],
 	DATENAME(weekday, order_approved_at) as [weekday],
 	case when DATEPART(hour, order_approved_at) >= 5 and DATEPART(hour, order_approved_at) < 12 then 'Morning'
 		when DATEPART(hour, order_approved_at) >= 12 and DATEPART(hour, order_approved_at) < 17 then 'Afternoon'
@@ -122,16 +100,39 @@ select
 from orders$
 left join order_sum on orders$.order_id = order_sum.order_id
 where order_status = 'delivered'
+and year(order_approved_at) = 2018
 )
 
 select 
 	top 1 concat([weekday],' ', time_period) as wd_time_period,
 	--count(distinct order_id) as sales,
 	--avg(total_price) as avg_order_value,
-	count(distinct order_id)*avg(total_price) as est_revenue
+	(count(distinct order_id)/count(distinct [Week])) * avg(total_price) as est_revenue
 from main
 group by [weekday], time_period
 order by est_revenue desc
+
+-- revenue by customer location
+
+with order_sum as (
+select 
+	order_id, 
+	sum(price) as total_price,
+	sum(freight_value) as shipping,
+	sum(price) + sum(freight_value) as total
+from order_items$ 
+group by order_id
+)
+
+select 
+	top 10 customer_city,
+	customer_state, 
+	sum(total_price) as total_revenue
+from order_sum
+left join orders$ on order_sum.order_id = orders$.order_id
+left join customers$ on orders$.customer_id = customers$.customer_id
+group by customer_state, customer_city
+order by total_revenue desc
 
 
 /* Analysis not included in Tableau Dashboard */
@@ -236,6 +237,29 @@ order by total_revenue desc
 -- avg_reviews
 
 select avg(review_score) as avg_review_score from order_reviews$
+
+-- yearly revenue
+
+with order_sum as (
+select 
+	order_id, 
+	sum(price) as total_price,
+	sum(freight_value) as shipping,
+	sum(price) + sum(freight_value) as total
+from order_items$ 
+group by order_id
+)
+
+select 
+	year(orders$.order_approved_at) as 'year',
+	sum(order_sum.total_price) as revenue
+from order_sum 
+left join orders$ on orders$.order_id = order_sum.order_id
+where 1=1
+and order_status = 'delivered'
+and orders$.order_approved_at is not null
+group by year(orders$.order_approved_at)
+order by 'year' desc
 
 
 
